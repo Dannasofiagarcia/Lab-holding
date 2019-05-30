6c49cf0457abb551a73ecd0fc82bfe02bca21005
Danna Garcia <dannasofiagarciatrujillo@hotmail.com>
2019-05-29 22:45:11 -0500

    Creating the method waterXtree with contracts

--- src/model/ManufacturingCompany.java
+++ src/model/ManufacturingCompany.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 public class ManufacturingCompany extends Company{
 
+	//Relaciones
+
+	private Product products;
 
 	//Constructor
 
@@ -11,6 +14,44 @@ public class ManufacturingCompany extends Company{
 		
 		super(nameCompany, nit, correspondanceDirection, phoneNumber, quantityEmployees, activeValue, companyType, nameLR, dateConstitution);
 
+	}
+
+	//Metodos
+
+	/**
+    * This method calculate how many trees company has to sembrate <br>
+
+    * <b>pre:</b> waterQuantity is initialized, waterQuantity != null <br>
+
+    * <b>post:</b> It has been calculate how many trees company should sembrate <br>
+
+    * @return String The method return a message with the information of how many trees company should sembrate <br>
+    */
+
+	public String waterXtree(){
+
+		String msg = "";
+		int valueToPay = 0;
+
+		if(products.getWaterQuantity() >= 1.0 && products.getWaterQuantity() <= 140.0){
+
+			valueToPay = 6;
+			msg = ("Los arboles que la empresa debe sembrar son " + valueToPay);
+		} 
+
+		else if (products.getWaterQuantity() >= 141.0 && products.getWaterQuantity() <= 800.0){
+
+			valueToPay = 25;
+			msg = ("Los arboles que la empresa debe sembrar son " + valueToPay);
+		}
+
+		else if (products.getWaterQuantity() > 800.0){
+
+			valueToPay = 200;
+			msg = ("Los arboles que la empresa debe sembrar son " + valueToPay);
+		}
+
+		return msg;
 	}
 
 }
